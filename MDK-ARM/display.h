1ae94cb3a8d5fba533c0dd54f787f4138e268650
Sergio Pastor Pérez <sergio.pastorperez@gmail.com>
2021-05-22 18:06:43 +0200

    Doomsday: Deleted project

--- MDK-ARM/display.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DISPLAY_H
-#define __DISPLAY_H
-
-void display_init (void);
-void display_show (void);
-
-#endif
-
-
--- MDK-ARM/test/retarget_io.d
+++
@@ -1,8 +0,0 @@
-test\retarget_io.o: C:\Users\juegos\AppData\Local\Arm\Packs\Keil\ARM_Compiler\1.6.3\Source\retarget_io.c
-test\retarget_io.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
-test\retarget_io.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-test\retarget_io.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-test\retarget_io.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
-test\retarget_io.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\assert.h
-test\retarget_io.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\rt_sys.h
-test\retarget_io.o: .\RTE\_test\RTE_Components.h
